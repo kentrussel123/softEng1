c0cd1c3d0ac8b60abf8d0234f372fbefc7524b0a
Kent Russel Balajadia <krb@egcextremeunrealtechnology.com>
2023-09-05 12:17:27 +0800

    initial commit

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+    public static void main(String args[]){
+        System.out.println("Hello, Software Engineering Course!")
+        System.out.println("My Name is BALAJADIA, Kent Russel")
+    }
+}
