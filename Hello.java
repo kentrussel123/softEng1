e878662c61a51ea9f6084d52d7af206f276b9b50
Kent Russel Balajadia <krb@egcextremeunrealtechnology.com>
2023-09-05 12:25:46 +0800

    added and line

--- Hello.java
+++ Hello.java
@@ -2,5 +2,8 @@ public class Hello{
     public static void main(String args[]){
         System.out.println("Hello, Software Engineering Course!")
         System.out.println("My Name is BALAJADIA, Kent Russel")
+
+        System.out.println ("Complicated world of SoftDev that is why it pays much!");
+
     }
 }
